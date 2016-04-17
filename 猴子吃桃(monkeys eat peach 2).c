2486b3319d30bbf6a64956a7329767febefaa0f7
MagnetoWang <1527672216@qq.com>
2016-04-17 16:07:48 +0800

    long long int 可以取更高位数的整型
    在取位数的时候，有很多数据类型有关long long还有--int64等等

--- 猴子吃桃(monkeys eat peach 2).c
+++ 猴子吃桃(monkeys eat peach 2).c
@@ -1,40 +1,40 @@
 #include<stdio.h>
-long double sequence_of_number[120];
-long double the_sum_sequence_of_number[120];
+long long int sequence_of_number[120];
+long long int the_sum_sequence_of_number[120];
  main()
 {
-	long double peach_number;
+	long long int peach_number;
 	int i;
-	sequence_of_number[0]=0.0;
-	sequence_of_number[1]=1.0;
-	the_sum_sequence_of_number[0]=0.0;
-	the_sum_sequence_of_number[1]=1.0;
-	printf("the_sum_sequence_of_number[1]=%Lf\n",the_sum_sequence_of_number[1]);
-	long double remainer;
-	scanf("%Lf",&the_sum_sequence_of_number[1]);
-	printf("%Lf\n",the_sum_sequence_of_number[1]);
-	while(scanf("%Lf",&peach_number)!=EOF)
+	sequence_of_number[0]=0;
+	sequence_of_number[1]=1;
+	the_sum_sequence_of_number[0]=0;
+	the_sum_sequence_of_number[1]=1;
+	//printf("the_sum_sequence_of_number[1]=%lld\n",the_sum_sequence_of_number[1]);
+	long long int remainer;
+	//scanf("%ld",&remainer);
+	//printf("%Ld\n",remainer);
+	while(scanf("%lld",&peach_number)!=EOF)
 	{
 
 		for (i = 2; the_sum_sequence_of_number[i-1]<=peach_number; )
 		{
-			printf("the_sum_sequence_of_number[i-1]=%Lf\n",the_sum_sequence_of_number[i-1]);
+			//printf("the_sum_sequence_of_number[i-1]=%Lf\n",the_sum_sequence_of_number[i-1]);
 			sequence_of_number[i]=sequence_of_number[i-2]+sequence_of_number[i-1];
-		    printf("sequence_of_number[i]=%Lf\n",sequence_of_number[i]);
+		   // printf("sequence_of_number[i]=%Lf\n",sequence_of_number[i]);
 			the_sum_sequence_of_number[i]=sequence_of_number[i]+the_sum_sequence_of_number[i-1];
 			i++;
 			/* code */
 		}
 		i--;
-		printf("i=%d\n",i);
+		//printf("i=%d\n",i);
 		remainer=peach_number-the_sum_sequence_of_number[i-1];
-		printf("the_sum_sequence_of_number[i-1]=%d\n",the_sum_sequence_of_number[i-1]);
+		//printf("the_sum_sequence_of_number[i-1]=%d\n",the_sum_sequence_of_number[i-1]);
 		if (remainer<=sequence_of_number[i-1])
 		{
-			printf("%d %.0Lf\n",i-1,sequence_of_number[i-1] );
+			printf("%d %lld\n",i-1,sequence_of_number[i-1] );
 			/* code */
 		}else{
-			printf("%d %.0Lf\n",i,remainer );
+			printf("%d %lld\n",i,remainer );
 		}
 	}
 	/* code */
