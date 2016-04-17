9be117c159ec727f839f35c9ebc8f73987e35878
MagnetoWang <1527672216@qq.com>
2016-04-18 00:09:26 +0800

    something wrong when i change the sort
    为什么稍微修改一下语法就有错误呢。我只是把他们移到if语句而已啊

--- 合并模板.cpp
+++ 合并模板.cpp
@@ -7,14 +7,15 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	std::vector<long long int> file_kb;
-	lont lont int one_number;
+	long long int one_number;//123456789123456789
 	int The_number_of_file,The_limitation_of_process;
 	while(cin>>The_number_of_file>>The_limitation_of_process)
 	{
-			long long int sum=0;
+	long long int sum=0;
 	for (int i = 0; i < The_number_of_file; i++)
 	{
 		cin>>one_number;
+		//cout<<one_number;
 		file_kb.push_back(one_number);
 	}
 	//int mod=The_number_of_file/The_limitation_of_process;
@@ -22,12 +23,20 @@ int main(int argc, char const *argv[])
 	sort(file_kb.begin(), file_kb.end());
 	if (The_number_of_file<=The_limitation_of_process)
 	{
-		sum=accumulate(file_kb.begin(), file_kb.end(),0);
-		cout<<sum<<endl;
+		 
+		//sum=accumulate(file_kb.begin(), file_kb.end(),0);something wrong
+		for(int i=0;i<The_number_of_file; i++)
+		{
+			sum=sum+file_kb[i];
+			cout<<sum<<endl;
+			cout<<file_kb[0]<<endl<<file_kb[1];
+		}
+		//cout<<file_kb[0]<<endl<<file_kb[1];
+		//cout<<sum<<endl;
 		printf("%llu\n",sum);
-		return 0;
+		//return 0;//NOTES:don't do that. you should end up with this programm by cin operation
 		/* code */
-	}
+	}else{
 	for (int i = 0; i < The_number_of_file; i++)
 	{
 		sum=sum+file_kb[i];
@@ -38,9 +47,9 @@ int main(int argc, char const *argv[])
 			/* code */
 		}
 	}
-	cout<<sum<<endl;
+	//cout<<sum<<endl;
 	printf("%llu\n",sum);
-		
+	}
 	}
 
 	return 0;
