f7f0f6c9dbe5fe5ea7ca1ca540f59d1449a8740c
MagnetoWang <1527672216@qq.com>
2016-04-17 15:50:13 +0800

    double long double 有问题

--- 猴子吃桃(monkeys eat peach 2).c
+++ 猴子吃桃(monkeys eat peach 2).c
@@ -1,31 +1,41 @@
 #include<stdio.h>
-int sequence_of_number[100];
-int the_sum_sequence_of_number[100];
-int main(int argc, char const *argv[])
+long double sequence_of_number[120];
+long double the_sum_sequence_of_number[120];
+ main()
 {
-	int peach_number;
-	sequence_of_number[0]=0;
-	sequence_of_number[1]=1;
-	the_sum_sequence_of_number[0]=0;
-	the_sum_sequence_of_number[1]=1;
-	int remainer;
-	while(scanf("%d",&peach_number)!=EOF)
+	long double peach_number;
+	int i;
+	sequence_of_number[0]=0.0;
+	sequence_of_number[1]=1.0;
+	the_sum_sequence_of_number[0]=0.0;
+	the_sum_sequence_of_number[1]=1.0;
+	printf("the_sum_sequence_of_number[1]=%Lf\n",the_sum_sequence_of_number[1]);
+	long double remainer;
+	scanf("%Lf",&the_sum_sequence_of_number[1]);
+	printf("%Lf\n",the_sum_sequence_of_number[1]);
+	while(scanf("%Lf",&peach_number)!=EOF)
 	{
-		for (int i = 2; the_sum_sequence_of_number[i]>=peach_number; i++)
+
+		for (i = 2; the_sum_sequence_of_number[i-1]<=peach_number; )
 		{
+			printf("the_sum_sequence_of_number[i-1]=%Lf\n",the_sum_sequence_of_number[i-1]);
 			sequence_of_number[i]=sequence_of_number[i-2]+sequence_of_number[i-1];
+		    printf("sequence_of_number[i]=%Lf\n",sequence_of_number[i]);
 			the_sum_sequence_of_number[i]=sequence_of_number[i]+the_sum_sequence_of_number[i-1];
+			i++;
 			/* code */
 		}
-		remainer=the_sum_sequence_of_number[i]-peach_number;
-		if (remainer<sequence_of_number[i-1])
+		i--;
+		printf("i=%d\n",i);
+		remainer=peach_number-the_sum_sequence_of_number[i-1];
+		printf("the_sum_sequence_of_number[i-1]=%d\n",the_sum_sequence_of_number[i-1]);
+		if (remainer<=sequence_of_number[i-1])
 		{
-			printf("%d %d\n",i-1,sequence_of_number[i-1] );
+			printf("%d %.0Lf\n",i-1,sequence_of_number[i-1] );
 			/* code */
 		}else{
-			printf("%d %d\n",i,remainer );
+			printf("%d %.0Lf\n",i,remainer );
 		}
 	}
 	/* code */
-	return 0;
 }
