a84ff0763ab7b9474172001577864e976c47ad0f
MagnetoWang <1527672216@qq.com>
2016-04-17 17:49:51 +0800

    algorithm model
    利用求余解决，而不是循环

---
+++ 抢人头.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	int wanshen,liangliang;
+	int kill;
+	int bleed;
+	while(scanf("%d%d%d",&wanshen,&liangliang,&bleed)!=EOF)
+	{
+		kill=wanshen+liangliang;
+		if (wanshen>bleed)
+		{
+		   		printf("wanshen\n");
+		}
+		int remainer=bleed%kill;
+		if (remainer==0)
+		{
+			printf("light\n");
+		}
+		else{
+			printf("wanshen\n");
+		}
+	}
+	return 0;
+}
