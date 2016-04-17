1bc9f6cccb398ba4406c35da675f5128b3fe2c2e
MagnetoWang <1527672216@qq.com>
2016-04-17 16:48:36 +0800

    输入出现问题
    getchar,scanf,puts以及最后输入完无法运行

--- 寻找万神.c
+++ 寻找万神.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include<string.h>
-char article[1000]
+char article[1000];
 int main(int argc, char const *argv[])
 {
-	int row,times=0;
+	int row,times=0,i;
 	scanf("%d",&row);
-	char sentence[100]
-	for (int i = 0; i < row; ++i)
+	getchar();
+	char sentence[100];
+	for (i = 0; i < row; i++)
 	{
+	    
 		gets(sentence);
+		//getchar();
 		strcat(article,sentence);
 		/* code */
+		puts(article);
 	}
+	puts(article);
+	getchar();
 	char wanshen[]="wanshen";
 	char *Pcopy=article;
-
-	while(Pfollow!=NULL)
+	while(Pcopy!=NULL)
 	{
 		Pcopy=strstr(Pcopy,wanshen);
 		Pcopy++;
-		times++
+		times++;
 	}
 	printf("%d\n",times );
-
-	
-	
-	/* code */
 	return 0;
 }
+/*wanshen
+wanshen
+wandfsf
+wadfsdw
+anshens*/
