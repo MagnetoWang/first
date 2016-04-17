1d6d0be50f856d48aea1e377c601d73ce8c098c9
MagnetoWang <1527672216@qq.com>
2016-04-17 16:24:19 +0800

    finished
    finacci sequence需要考虑前3个数的特殊性，之前没有对此进行分类，所以答案错误
    http://acm.xidian.edu.cn/problem.php?cid=1022&pid=1

--- 猴子吃桃(monkeys eat peach 2).c
+++ 猴子吃桃(monkeys eat peach 2).c
@@ -15,7 +15,11 @@ long long int the_sum_sequence_of_number[120];
 	//printf("%Ld\n",remainer);
 	while(scanf("%lld",&peach_number)!=EOF)
 	{
-
+	    if(peach_number==1||peach_number==2||peach_number==3)
+	    {
+	    	printf("1 1\n");
+	    	continue;
+	    }
 		for (i = 2; the_sum_sequence_of_number[i-1]<=peach_number; )
 		{
 			//printf("the_sum_sequence_of_number[i-1]=%Lf\n",the_sum_sequence_of_number[i-1]);
@@ -31,10 +35,10 @@ long long int the_sum_sequence_of_number[120];
 		//printf("the_sum_sequence_of_number[i-1]=%d\n",the_sum_sequence_of_number[i-1]);
 		if (remainer<=sequence_of_number[i-1])
 		{
-			printf("%d %lld\n",i-1,sequence_of_number[i-1] );
+			printf("%d %llu\n",i-1,sequence_of_number[i-1] );
 			/* code */
 		}else{
-			printf("%d %lld\n",i,remainer );
+			printf("%d %llu\n",i,remainer );
 		}
 	}
 	/* code */
