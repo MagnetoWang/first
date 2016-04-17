4dfc38b3a5475d13e7f358d648bc5cc1f4e031ce
MagnetoWang <1527672216@qq.com>
2016-04-17 16:47:31 +0800

    初始模样

---
+++ 寻找万神.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<string.h>
+char article[1000]
+int main(int argc, char const *argv[])
+{
+	int row,times=0;
+	scanf("%d",&row);
+	char sentence[100]
+	for (int i = 0; i < row; ++i)
+	{
+		gets(sentence);
+		strcat(article,sentence);
+		/* code */
+	}
+	char wanshen[]="wanshen";
+	char *Pcopy=article;
+
+	while(Pfollow!=NULL)
+	{
+		Pcopy=strstr(Pcopy,wanshen);
+		Pcopy++;
+		times++
+	}
+	printf("%d\n",times );
+
+	
+	
+	/* code */
+	return 0;
+}
